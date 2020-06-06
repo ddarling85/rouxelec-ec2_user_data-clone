c3d84785a8abc2299e2a11be551affeba3086d0d
rouxelec <francois.rouxel@slalom.com>
2020-06-06 09:48:18 -0400

    Update userdata.py

--- userdata.py
+++ userdata.py
@@ -9,6 +9,7 @@ dynamodb = boto3.resource('dynamodb', region_name='ca-central-1')
 
 cidr_range_table = dynamodb.Table('cidr_range_table')
 response = cidr_range_table.scan()
+ping_test="1 packets transmitted, 1 received, 0% packet loss"
 for item in response['Items']:
     print(item)
     ip=item.get('ec2_ip_address')
@@ -18,13 +19,16 @@ for item in response['Items']:
            stdout=subprocess.PIPE, 
            stderr=subprocess.STDOUT)
         stdout,stderr = out.communicate()
+        result_ping="PING NOT OK"
+        if ping_test in str(stdout):
+            result_ping="PING OK"
         response = cidr_range_table.update_item(
             Key={
                 'id': vpc_name
             },
             UpdateExpression="SET ping_from_"+str(ip.replace('.',"_"))+" = :r",            
             ExpressionAttributeValues={
-                ':r':  stdout,
+                ':r':  result_ping,
             },
             ReturnValues="UPDATED_NEW"
         )
