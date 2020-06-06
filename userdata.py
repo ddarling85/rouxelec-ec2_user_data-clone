c3b8bd72c6c5b2ad6b4ff0d40e2a2523858a98e9
rouxelec <francois.rouxel@slalom.com>
2020-06-06 10:26:25 -0400

    Update userdata.py

--- userdata.py
+++ userdata.py
@@ -1,3 +1,4 @@
+import socket
 import boto3
 import time
 import json
@@ -7,33 +8,32 @@ ec2 = boto3.resource('ec2', region_name='ca-central-1')
 client = boto3.client('ec2', region_name='ca-central-1')
 dynamodb = boto3.resource('dynamodb', region_name='ca-central-1')
 
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
+print("Your Computer Name is:" + hostname)
+
+
 cidr_range_table = dynamodb.Table('cidr_range_table')
 response = cidr_range_table.scan()
 ping_test="1 packets transmitted, 1 received, 0% packet loss"
-cmd_local_ip="curl http://169.254.169.254/latest/meta-data/local-ipv4"
-out = subprocess.Popen([cmd_local_ip], 
-           stdout=subprocess.PIPE, 
-           stderr=subprocess.STDOUT)
-        stdout,stderr = out.communicate()
-local_ip=str(stdout)    
-for item in response['Items']:
-    print(item)
+local_ip=IPAddr.replace('.',"_")
+for item in response['Items']:    
     ip=item.get('ec2_ip_address')
     vpc_name=item.get('id')
     if not ip is None:
-        if not ip:
-            out = subprocess.Popen(['ping', ip, '-c', '1'], 
-            stdout=subprocess.PIPE, 
+        if ip:            
+            out = subprocess.Popen(['ping', ip, '-c', '1'],
+            stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT)
             stdout,stderr = out.communicate()
             result_ping="PING NOT OK"
             if ping_test in str(stdout):
-                result_ping="PING OK"
+                result_ping="PING OK"            
             response = cidr_range_table.update_item(
                 Key={
                     'id': vpc_name
                 },
-                UpdateExpression="SET ping_from_"+str(local_ip.replace('.',"_"))+" = :r",            
+                UpdateExpression="SET ping_from_"+local_ip+" = :r",
                 ExpressionAttributeValues={
                     ':r':  result_ping,
                 },
